44242b6c20731958bd5507c0fec1fe56f3862bd7
wolfenergistics <leederdominic@gmail.com>
2020-10-01 08:22:23 +0000

    finalisation du tp3

---
+++ tp3/aleatoire.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class alea4 {
+    public static void main(String[] arg) {
+        final Scanner clavier = new Scanner(System.in);
+        double yolo;int entre;int i;double min;double max;double[] tab;
+
+     System.out.println("entrer une valeur");
+     entre = clavier.nextInt();
+     tab = new double[entre];
+    
+        for(i=0;i<(tab.length);i++){
+            yolo = Math.random()*10-5;
+            System.out.println(yolo);
+            tab[i] = yolo;
+
+        }
+     min = tab[0];
+     max = tab[entre-1];
+        for (i=0;i<(tab.length);i++){
+            if(min>tab[i]){
+                min = tab[i];
+            }
+            if(max<tab[i]){
+                max = tab[i];
+            }   
+
+        }
+
+        System.out.println("le minimum est "+min+" et le maximum est "+max);
+    }
+}
