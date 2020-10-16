4dce3763eb6ade43dbb87964879feeefee533d27
wolfenergistics <71070849+wolfenergistics@users.noreply.github.com>
2020-10-16 05:45:50 +0200

    fin facturation
    début triangle pascal du tp5

--- tp4/facturation/facturation.java
+++ tp4/facturation/facturation.java
@@ -1,16 +1,34 @@
 import java.util.Scanner;
 
+import javax.xml.crypto.dsig.SignatureProperties;
+
 public class facturation {
     private static Scanner clavier = new Scanner(System.in);
     public static void main(String[] arg){
-     int siropera; int siropbleu;  
+     int[] siropera = new int[4]; int siropbleu;  double fdt;String facture;
      
-     siropera = saisirNbSiropErable();
-     System.out.println("nombre commander: "+siropera);
      
-     siropbleu = saisirNbSiropBleuets();
-     System.out.println("nombre commander: "+siropbleu); 
-       
+     System.out.println("voulez-vous saisir une facture?");
+     System.out.println("oui/non");
+     facture = clavier.nextLine();
+     
+        while(facture.equals("oui")){
+      
+            siropera = saisirNbSiropErable();
+            siropbleu = saisirNbSiropBleuets();
+            fdt = calculerFraisTransport(siropera, siropbleu);
+            afficherFacture(siropera, siropbleu); 
+            System.out.println();
+            System.out.println("voulez-vous saisir une facture?");
+            System.out.println("oui/non");
+            facture = clavier.nextLine();
+        
+        }
+     
+        
+     System.out.println("bonne fin de journee");
+
+
     }
     
 
@@ -33,25 +51,82 @@ public class facturation {
 
 
 
-    public static  int saisirNbSiropErable(){
-        int siroperable;
-        boolean verifsirop = false;
+    public static  int[] saisirNbSiropErable(){
+        int[] tab;int siroperabletot;String siroptype="a definir";int siropmedium = 1;int siropclair=1;int siropnorm=1;
+        boolean verifsirop;
         
-        System.out.println("saisir nombre de sirop erable");
-        
-        siroperable = clavier.nextInt();
-       
-        while (verifsirop == false){
-            if (siroperable%3 != 0){
-              verifsirop = false;
-              System.out.println("vendu par lot de 3 merci de corriger");
-              siroperable = clavier.nextInt();
+        while(!siroptype.equals("fin")){
+            System.out.println("saisir type de sirop: normal/clair/medium fin pour arreter la saisie :");
+            siroptype = clavier.nextLine();
+            verifsirop = false;
+            switch (siroptype){
+                
+                case "normal" :
+                    System.out.println("saisir nombre de cruchon");
+                    siropnorm = clavier.nextInt();
+  
+                 while (verifsirop == false){
+                    if (siropnorm%3 != 0){
+                        verifsirop = false;
+                        System.out.println("vendu par lot de 3 merci de corriger");
+                        siropnorm = clavier.nextInt();
+                    }
+                    else{
+                        verifsirop = true;
+                    }
+                 }
+                break;
+                    
+                case "clair" :
+                    System.out.println("saisir nombre de cruchon");
+                    siropclair = clavier.nextInt();
+   
+                    while (verifsirop == false){
+                        if (siropclair%3 != 0){
+                            verifsirop = false;
+                            System.out.println("vendu par lot de 3 merci de corriger");
+                            siropclair = clavier.nextInt();
+                        }
+                        else{
+                            verifsirop = true;
+                        }
+                    }
+                break;
+                
+                case "medium" :
+                    System.out.println("saisir nombre de cruchon");
+                    siropmedium = clavier.nextInt();
+
+                    while (verifsirop == false){
+                        if (siropmedium%3 != 0){
+                            verifsirop = false;
+                            System.out.println("vendu par lot de 3 merci de corriger");
+                            siropmedium = clavier.nextInt();
+                        }
+                        else{
+                            verifsirop = true;
+                        }
+                    }
+                break;
+                
+                    
+                
+            } 
+        }  
+            if (siropnorm == 1){
+                siropnorm = 0;
             }
-            else{
-                verifsirop = true;
-            }              
-        }
-        return siroperable;
+            if(siropclair == 1){
+                siropclair =0;
+            }
+            if(siropmedium ==1){
+                siropmedium =0;
+            }
+      
+      siroperabletot = siropmedium + siropclair + siropnorm;
+      tab =new int[] { siropnorm , siropclair , siropmedium , siroperabletot };
+     
+      return  tab;
     }
 
     public static int saisirNbSiropBleuets(){
@@ -67,18 +142,45 @@ public class facturation {
         return siropbleuets;
     }
 
-    public static double calculerFraisTransport(int siropera,int siropbleu){
-        double fraisdetransporterable;double fraisdetransportbleu;
+    public static double calculerFraisTransport(int[] siropera,int siropbleu){
+        double fraisdetransporterable;double fraisdetransportbleu;double fraistot;
 
-        switch (siropera) {
-            case (siropera < 11 ): 
-             fraisdetransporterable = siropera*1.5;
-            break;
+        
+            if (siropera[3] <= 11){
+             fraisdetransporterable = siropera[3]*1.5;
+            }
 
-            case (siropera<24)):
+            else if (siropera[3]<=24){
+             fraisdetransporterable = siropera[3]*1;
+            }
+            
+            else{
+            fraisdetransporterable = 0;
+            }
              
 
-        }
+        
+        fraisdetransportbleu = siropbleu *0.5; 
+        fraistot = fraisdetransportbleu +fraisdetransporterable;
+        return fraistot;
     }
+
+    public static void  afficherFacture(int[] siropera,int siropbleu){
+        double prixnorm;double prixbleu;double prixtot;double prixclair;double prixmedium;
+
+        prixnorm = 28*siropera[0];
+        prixclair = 23.5*siropera[1];
+        prixmedium = 21.4*siropera[2];
+        prixbleu = 12*siropbleu;
+        prixtot = prixbleu + prixnorm + prixclair + prixmedium + calculerFraisTransport(siropera, siropbleu);
+        System.out.println("vous avez commandez : "+siropera[0]+" normal/aux vendu a 28e/L soit :"+prixnorm);
+        System.out.println("vous avez commandez : "+siropera[1]+" clair vendu a 23.5e/L soit :"+prixclair);
+        System.out.println("vous avez commandez : "+siropera[2]+" medium vendu a 21.4e/L soit :"+prixmedium);
+        System.out.println("vous avez commandez : "+siropbleu+" vendu a 12e/bouteille soit : "+prixbleu);
+        System.out.println("les frais de port s'elevent a : "+calculerFraisTransport(siropera, siropbleu));
+        System.out.println("la facture est donc de : "+prixtot);
+        
+    }
+
 }
 
--- tp5/TrianglePascal.java
+++ tp5/TrianglePascal.java
@@ -2,14 +2,19 @@ import java.util.Scanner;
 
 public class TrianglePascal {
     private static Scanner clavier = new Scanner(System.in);
+    
     public static void main(String[] arg){
-        double facto;
+        
+        int n;
+        System.out.println("entrer n : ");
+        n = clavier.nextInt();
+        
 
-        facto = factorielle();
-        System.out.println(facto);
+  
 
-
-
+      
+        
+        afficherTriangle(n);
     }
     
 
@@ -21,15 +26,44 @@ public class TrianglePascal {
 
 
 
-    public static double factorielle(){
-        double tempo;
-        int x;
+    public static int factorielle(int x){
+        int tempo;
+        
         tempo = 1;
-        x = clavier.nextInt();
-        for(int i =1; i<=x ; i++ ){
+        
+       
+        for(int i=1;i<=x;i++){
         tempo = tempo * i;
         }
         
         return tempo;
+    }
+
+    public static int coeffBinomial(int n,int p){
+       int binom;int facton;int factodiv;int nmp = n-p;int factonmp;
+
+        facton = factorielle(n);
+        factonmp = factorielle(nmp);
+        factodiv = factorielle(p);
+        
+        binom = facton/(factonmp*factodiv);
+        return binom;
+
+    }
+    public static int afficherLigne(int n){
+        int ligne = 0;
+        for (int i=0;i<=n;i++){
+            ligne = coeffBinomial(n,i);
+         System.out.print(ligne + " ");  
+        }
+        return ligne;
+    }
+    public static int afficherTriangle(int n){
+        int triangle = 0;
+        for(int i=0;i<=n;i++){
+            triangle = afficherLigne(i);
+            System.out.println(triangle+" ");
+        }
+        return triangle;
     }
 }
